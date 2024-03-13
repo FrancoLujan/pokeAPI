ad965ee3b834f976ef36e71311106cebf6123708
franco lujan <francolujann1@gmail.com>
2024-03-13 14:37:34 -0300

    mejoras y implementado una circulo de carga

--- js/main.js
+++ js/main.js
@@ -1,37 +1,62 @@
 const pokemon = document.querySelector("#buscador");
 const botonBuscar = document.querySelector("#botonBuscar");
-const habilidades = document.querySelector("#habilidades");
-const imagenPokemon = document.querySelector("#cajaImagen img");
 
-async function BuscarPokemon(Pokemon) {
-  let sombraAnterior = pokemon.style.boxShadow;
+let cajaCarta = document.querySelector(".cajaCarta");
+const copia = cajaCarta.innerHTML;
+
+
+async function BuscarPokemon(Pokemon, copia) {
+  let devolver = "error"
   try {
-    let promesa = await fetch(`https://pokeapi.co/api/v2/pokemon/${Pokemon.toLowerCase()}`);
+    let promesa = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${Pokemon.toLowerCase()}`
+    );
     let resolucion = await promesa.json();
-
-    imagenPokemon.setAttribute("src", resolucion.sprites["front_default"]);
-    habilidades.innerHTML = "";
-    
-    let listaHabilidades = resolucion["abilities"];
-
-    listaHabilidades.forEach((poder, i) => {
-      habilidades.innerHTML += `<p>Habilidad ${i}: <strong>${poder.ability.name}</strong></p>`;
-    });
-    let tipo = resolucion["types"];
-    habilidades.innerHTML += `<p>Experiencia: <strong>${resolucion["base_experience"]}</strong> </p>\
-    <p>Altura: <strong>${resolucion["height"]}</strong></p>\
-    <p>Peso: <strong>${resolucion["weight"]} </strong>\
-    <p>Tipo: <strong>${tipo[0].type.name}</strong></p>`;
+    devolver = resolucion
+   
+    return devolver;
   } catch (error) {
-    imagenPokemon.setAttribute("src", "imagenes/zoom-question.png");
-        habilidades.innerHTML = `<p> El pokemon "${Pokemon}" no existe</p>`
+    
+
+    return devolver;
   }
 }
 
-botonBuscar.addEventListener("click", (e) => {
+function muestraPokemon(resolucion, nombrePokemon) {
+  cajaCarta.innerHTML = copia;
+  const imagenPokemon = document.querySelector("#cajaImagen img");
+  const habilidades = document.querySelector("#habilidades");
+
+  try {
+    if (resolucion != "error") {
+      imagenPokemon.setAttribute("src", resolucion.sprites["front_default"]);
+      habilidades.innerHTML = "";
+
+      let listaHabilidades = resolucion["abilities"];
+
+      listaHabilidades.forEach((poder, i) => {
+        habilidades.innerHTML += `<p>Habilidad ${i}: <strong>${poder.ability.name}</strong></p>`;
+      });
+      let tipo = resolucion["types"];
+      habilidades.innerHTML += `<p>Experiencia: <strong>${resolucion["base_experience"]}</strong> </p>\
+    <p>Altura: <strong>${resolucion["height"]}</strong></p>\
+    <p>Peso: <strong>${resolucion["weight"]} </strong>\
+    <p>Tipo: <strong>${tipo[0].type.name}</strong></p>`;
+    }else{
+      imagenPokemon.setAttribute("src", "imagenes/zoom-question.png");
+      habilidades.innerHTML = `<p> El pokemon "${nombrePokemon}" no existe</p>`
+    }
+  } catch (error) {}
+}
+
+botonBuscar.addEventListener("click", async (e) => {
   e.preventDefault();
   let nombrePokemon = pokemon.value;
   if (nombrePokemon != "") {
-    BuscarPokemon(nombrePokemon);
+    cajaCarta.innerHTML = "<div class=spinner ></div>";
+    let resultado = await BuscarPokemon(nombrePokemon, copia);
+
+
+    muestraPokemon(resultado, nombrePokemon);
   }
 });
